5b328b6dd83ba98099f64fb2b5299e8fec8dbdff
ankon <ankon@ankon-mac.local>
2017-08-31 14:19:51 +0800

    4th

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,31 @@
 Rails.application.routes.draw do
-  namespace :api, :defaults => { :format => :json } do
-    namespace :v1 do
-      get "/trains" => "trains#index", :as => :trains
-      get "/trains/:train_number" => "trains#show", :as => :train
 
-      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
-      post "/reservations" => "reservations#create", :as => :create_resrvations
-      patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
-      delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
-    end
-  end
-  
+  devise_for :users
+  namespace :api, :defaults => { :format => :json } do
+   namespace :v1 do
+
+     get "/trains"  => "trains#index", :as => :trains
+     get "/trains/:train_number" => "trains#show", :as => :train
+
+     get "/reservations" => "reservations#index", :as => :reservations
+     get "/reservations/:booking_code" => "reservations#show", :as => :reservation
+     post "/reservations" => "reservations#create", :as => :create_reservations
+     patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
+     delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+
+     post "/signup" => "auth#signup"
+     post "/login" => "auth#login"
+     post "/logout" => "auth#logout"
+   end
+ end
+
   resources :cities do
     member do
       post :update_temp
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "welcome#index"
+
 end
